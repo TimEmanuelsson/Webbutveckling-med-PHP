2c5cdb14b01d39d0812984a4bfc22d493d214df7
TimEmanuelsson <te222ds@student.lnu.se>
2014-10-25 00:09:51 +0200

    fix
    fix

--- Projekt/Applikation/src/navigation/model/databaseConnectionSettings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class settings{
-	public static $DB_USERNAME = 'jockepocke_se';
-	public static $DB_PASSWORD = 'WZdpQ8Fx';
-	public static $DB_CONNECTION = 'mysql:host=jockepocke.se.mysql;dbname=jockepocke_se';
-	public static $DB_ROOT_PATH = "";
-}
